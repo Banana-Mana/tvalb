741a1830fdc4f7ca2d472350ce9dd294f0260f9b
Доан Kutbay <dogank@setplex.com>
2017-08-15 20:09:35 +0300

    Add files via upload

---
+++ isScrolledIntoView.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Don Corleone on 04-Aug-17.
+ */
+$.fn.isOnScreen = function(){
+
+    var win = $(window);
+
+    var viewport = {
+        top : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+};
